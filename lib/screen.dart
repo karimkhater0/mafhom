4ba200ad8b2273cf77d22af6a3e63e06d71f6cba
karim <kimokhater137@gmail.com>
2024-02-05 17:18:49 +0200

    commit

--- lib/screen.dart
+++ lib/screen.dart
@@ -3,22 +3,19 @@ import 'package:flutter/material.dart';
 import 'curves2.dart';
 
 class TestCurves extends StatelessWidget {
-   TestCurves({super.key});
+  TestCurves({super.key});
   var width = 400.0;
-  late var height =width*1.0638297872340425;
+  late var height = width * 1.0638297872340425;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-
-
         child: Center(
           child: CustomPaint(
-            size: Size(width , height), //You can Replace [WIDTH] with your desired width for Custom Paint and height will be calculated automatically
+            size: Size(width,
+                height), //You can Replace [WIDTH] with your desired width for Custom Paint and height will be calculated automatically
             painter: RPSCustomPainter(),
           ),
         ),
